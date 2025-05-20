a3efbc3382e6ad4e3fe8dde5e4618fd005f4049a
Artemis <artemiswilkinson@hotmail.com>
2025-05-20 15:10:22 +0100

    Update Projects.js
    Fix typos and errors.

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -18,17 +18,15 @@ export default function Projects() {
         })}
         <div className="currentProject">
           <h4>Current Projects</h4>
-          <p> I am currently interning at Digital Edge and working on their internal projects.</br>
+          <p> I am currently interning at Digital Edge and working on their internal projects.<br />
           Currently, I am coding the MVP for their CRM (client resource management) system alongside their Senior Developer. This has allowed me to use my previous experience in administration to provide ideas combined with my coding skills to build something that will improve workflows, and provide a scaleable enviroment for Digital Edge and future customers.
-          I am working with Wix Velo and Studio, as well as their database system which is powered by MongoDB. </br>
-          I have been designing as well as creating forms, working on the project, payment and client pages, as well as providing testing and feedback to their Senior Developer. </br> 
+          I am working with Wix Velo and Studio, as well as their database system which is powered by MongoDB. <br />
+          I have been designing as well as creating forms, working on the project, payment and client pages, as well as providing testing and feedback to their Senior Developer. <br /> 
           I have been immensely enjoying working on professional products, but this has unfortunately slowed down my personal projects! I will provide updates and links when I can.
           <p/>
           <p>
-          My first Wix project was completed over the course of a few days in March as a technical test for Digital Edge. 
-            working on smaller projects to keep my coding skills sharp and interning at Digital Edge to get experience coding in a professional enviroment. 
-              <br />
-            <a href="https://artemisswilkinson.wixstudio.com/360business">Click here to see my current WIP</a>
+          My first Wix project was completed over the course of a few days in April as a technical test for Digital Edge. <br />
+            <a href="https://artemisswilkinson.wixstudio.com/360business">Click here to see the MVP</a>
           </p>
               
           <p>
