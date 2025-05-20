e8741be7efb413c8cc448bcae496976b79e45956
Artemis <artemiswilkinson@hotmail.com>
2025-05-20 15:36:55 +0100

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -20,12 +20,12 @@ export default function Projects() {
           <h4>Current Projects</h4>
           <p> 
           I am currently interning at <a href="https://www.wearedigitaledge.co.uk/">Digital Edge</a> and working on their internal projects.
-          <br>
+          <br/>
           At the Moment, I am coding the MVP for their CRM (client resource management) system alongside their Senior Developer. This has allowed me to use my previous experience in administration to provide ideas combined with my coding skills to build something that will improve workflows, and provide a scaleable enviroment for Digital Edge and future customers.
           I am working with Wix Velo and Studio, as well as their database system which is powered by MongoDB. 
-          <br>
+          <br/>
           I have been designing as well as creating forms, working on the project, payment and client pages, as well as providing testing and feedback to their Senior Developer. 
-          <br> 
+          <br/> 
           I have been immensely enjoying working on professional products, but this has unfortunately slowed down my personal projects! I will provide updates and links when I can.
           </p>
           
