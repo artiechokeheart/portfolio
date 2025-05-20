80a567c56eedd5829f3712d486b2feee18b8deb4
Artemis <artemiswilkinson@hotmail.com>
2025-05-20 15:57:57 +0100

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -20,20 +20,21 @@ export default function Projects() {
           <h4>Current Projects</h4>
           <p> 
           I am currently interning at <a href="https://www.wearedigitaledge.co.uk/">Digital Edge</a> and working on their internal projects.
-          <br/>
+          </p> 
+          <p>
           At the Moment, I am coding the MVP for their CRM (client resource management) system alongside their Senior Developer. This has allowed me to use my previous experience in administration to provide ideas combined with my coding skills to build something that will improve workflows, and provide a scaleable enviroment for Digital Edge and future customers.
           I am working with Wix Velo and Studio, as well as their database system which is powered by MongoDB. 
-          <br/>
+          <
           I have been designing as well as creating forms, working on the project, payment and client pages, as well as providing testing and feedback to their Senior Developer. 
-          <br/> 
+          </p>
+          <p>
           I have been immensely enjoying working on professional products, but this has unfortunately slowed down my personal projects! I will provide updates and links when I can.
           </p>
-          
           <p>
           My first Wix project was completed over the course of a few days in April as a technical test for Digital Edge. <br />
             <a href="https://artemisswilkinson.wixstudio.com/360business">Click here to see the MVP</a>
           </p>
-              
+          <br>
           <p>
             I am working on a long-term project that will allow users to
             catalogue their boardgame collections, store information on where
